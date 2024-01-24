66f8190b0692b5bb1051fff5d99003ebba4fb907
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-24 19:17:13 +0000

    displayCountries function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 //1. Create an async function called getCountries
-
-
-//  - pass the data to the displayCountries function.
-//  - Catch any errors and log them to the console.
 async function getCountries() {
     try {
         //  - retrieve the name, capital, population and flags for all countries.
@@ -13,18 +9,41 @@ async function getCountries() {
         //  - Convert the response to JSON.
         const data = await response.json();
 
+        //  - pass the data to the displayCountries function.
         console.log(data);
+        displayCountries(data);
 
     } catch (error) {
+        //  - Catch any errors and log them to the console.
         console.error(error)
     }
 }
 
 //2. Create a displayCountries function that takes in an array of countries.
-//  - Loop over the array of countries.
-//      - Create a div for each country.
-//      - Add the country name and flag to the div with the provided HTML structure.
-//      - Add the created div to the `.countries` container element.
+function displayCountries(data) {
+    //  - Loop over the array of countries.
+    data.forEach( (country) => {
+        //      - Create a div for each country.
+        //      - Add the country name and flag to the div with the provided HTML structure.
+        let countryHTML = `
+            <div class="country">
+                <h3 class="country-name">${country.name.common}</h3>
+                <img class="country-flag" src="https://flagcdn.com/nl.svg" />
+                <div class="content">
+                <h3>Capital</h3>
+                <p>Amsterdam</p>
+                <h3>Population</h3>
+                <p>16.655.799</p>
+                <h3>Region</h3>
+                <p>Europe</p>
+                </div>
+            </div>
+        `;
+
+        //      - Add the created div to the `.countries` container element.
+        document.querySelector('.countries').insertAdjacentHTML('beforeend', countryHTML);
+    });
+}
 
 //3. Call the getCountries function.
-getCountries()
+getCountries();
