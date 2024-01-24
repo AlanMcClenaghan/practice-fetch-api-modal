76ff3a90e2236763c3082833e136e56351921674
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-24 18:59:04 +0000

    getCountries function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 //1. Create an async function called getCountries
-//  - retrieve the name, capital, population and flags for all countries.
-//  - Convert the response to JSON.
+
+
 //  - pass the data to the displayCountries function.
 //  - Catch any errors and log them to the console.
+async function getCountries() {
+    try {
+        //  - retrieve the name, capital, population and flags for all countries.
+        const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region');
+
+        if (!response.ok) throw new Error('Something went wroing');
+
+        //  - Convert the response to JSON.
+        const data = await response.json();
+
+        console.log(data);
+
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 //2. Create a displayCountries function that takes in an array of countries.
 //  - Loop over the array of countries.
@@ -11,3 +27,4 @@
 //      - Add the created div to the `.countries` container element.
 
 //3. Call the getCountries function.
+getCountries()
